aa12e689e5526c6778e65b739223ed87cd4b975a
Group <groupzakub@gmail.com>
2021-12-10 09:36:25 +0700

    test

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <div>Hellp</div>
         <a
           className="App-link"
           href="https://reactjs.org"
