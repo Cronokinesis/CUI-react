89cc5d7c558009ccc95d7129693a85079004d505
Group <groupzakub@gmail.com>
2021-12-11 12:34:35 +0700

    remove comment

--- src/components-hook/common/Clock.jsx
+++ src/components-hook/common/Clock.jsx
@@ -1,47 +1,4 @@
-import { Component, useEffect, useState } from 'react';
-
-// class ClockHook extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { date: new Date() };
-//     console.log('initial');
-//   }
-
-//   static getDerivedStateFromProps(props, state) {
-//     console.log('props =>', props);
-//     console.log('state => ', state);
-//     return { title: props.title };
-//   }
-
-//   componentDidMount() {
-//     this.timerID = setInterval(() => this.tick(), 1000);
-//     console.log('Clock => componentDidMount');
-//   }
-
-//   componentDidUpdate() {
-//     console.log('Clock => componentDidUpdate');
-//   }
-
-//   componentWillUnmount() {
-//     clearInterval(this.timerID);
-//     console.log('Clock => componentWillUnmount');
-//   }
-
-//   tick() {
-//     this.setState({
-//       date: new Date(),
-//     });
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <h1>Hello, world!</h1>
-//         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-//       </div>
-//     );
-//   }
-// }
+import { useEffect, useState } from 'react';
 
 const ClockHook = () => {
   const [date, setDate] = useState(new Date());
@@ -58,7 +15,7 @@ const ClockHook = () => {
     };
   }, []);
 
-  useEffect(()=> {
+  useEffect(() => {
     console.log('Clock Hook => componentDidUpdate');
   }, [date]);
 
